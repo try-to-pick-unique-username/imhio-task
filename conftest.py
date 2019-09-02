12d6bf0603cc1ea1f045dda20ed18b66a0613e95
Alesia Kovalevych <alesyakovalevich@gmail.com>
2019-09-02 15:28:39 +0300

    Final cleanup

--- conftest.py
+++ conftest.py
@@ -7,11 +7,3 @@ from application import Application
 def app():
     application = Application()
     return application
-
-
-# @pytest.fixture(scope='session')
-# def company_setup(app, request):
-#     app.setup_company()
-#     def company_teardown():
-#         app.clean_company()
-#     request.addfinalizer(company_teardown)
--- run.sh
+++ run.sh
@@ -4,6 +4,5 @@ BASEDIR=$(pwd)
 ALLUREDIR=$BASEDIR/allure_reports
 
 pytest -v -m API --alluredir=$ALLUREDIR
-#pytest $BASEDIR/tests/webhook_test.py -v --alluredir=$BASEDIR/allure_reports
 
 allure serve $ALLUREDIR
